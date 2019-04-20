c9092033b5c1a0ee41ec5664a80f74c67504f2c1
kellymears <code@kellymears.me>
2019-04-19 21:48:49 -0700

    FIX: remove set error handler calls

--- ActionNetwork.php
+++ ActionNetwork.php
@@ -188,11 +188,6 @@ class ActionNetwork
 
     public static function error($error)
     {
-        if (_DEBUG_) {
-            set_error_handler('debug_error_handler');
-        } else {
-            set_error_handler('nice_error_handler');
-        }
         trigger_error($error, E_USER_ERROR);
     }
 }
